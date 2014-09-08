5daeaa5c6134401bd72c3d0c4f1f06de10328bd2
Gerardo Moad <gerardo@gerardomoad.com>
2014-09-08 12:25:32 -0700

    improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CWD=`pwd`;
+
 for file in `ls -lad .*`;
 do
     if [[ "x." == "x$file" || "x.." == "x$file" || "x.git" == "x$file" ]];
@@ -21,7 +23,7 @@ do
             then
                 rm -rf "$HOME/$file";
             fi;
-            ln -F -i -s $file "$HOME";
+            ln -F -i -s "$CWD/$file" "$HOME";
         else
             echo "not touching $file";
         fi;
@@ -43,6 +45,21 @@ do
         fi;
     fi;
 done;
+
+create_link=1;
+file="pathogen.vim";
+if [ -e "$HOME/.vim/autoload/$file" ];
+then
+    read -p "regular file $file already exists in your home dir. overwrite it? [y/n]" overwrite;
+    create_link=`echo $overwrite | grep -i "^y"` ;
+fi;
+if [ $create_link ];
+then
+    echo "overwriting $file";
+    ln -i "vim-pathogen/autoload/$file" "$HOME/.vim/autoload/$file";
+else
+    echo "not touching $file";
+fi;
 
 #mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 #curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
